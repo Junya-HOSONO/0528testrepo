bfac43889fa341d93cde6e52d05d37dae9bb7614
Junya HOSONO <junya.hosono@gmail.com>
2024-12-21 01:32:15 +0000

    システム環境を取得できて、docker環境で実行してることが確認できた。すげー

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+import sys
+import platform
+
 for i in range(1, 10):
     for j in range(1, 10):
         print(f"{i} x {j} = {i * j}")
     print()
-    
+
+print (sys.version)
+print (platform.platform())
+
